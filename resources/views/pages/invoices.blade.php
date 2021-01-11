f5c2f257d061b8db9eb05c53e28c6fc77f5b9507
Gabriel Zambrano <gab.zambrano@gmail.com>
2021-01-12 00:40:36 +0100

    invoices

--- resources/views/pages/invoices.blade.php
+++ resources/views/pages/invoices.blade.php
@@ -10,15 +10,50 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <section class="p-8">
-                    <table>
-                        @foreach ($invoices as $invoice)
-                        <tr>
-                            <td>{{ $invoice->date()->toFormattedDateString() }}</td>
-                            <td>{{ $invoice->total() }}</td>
-                            <td><a href="/user/invoice/{{ $invoice->id }}">Download</a></td>
-                        </tr>
-                        @endforeach
-                    </table>
+                    <div class="flex flex-col">
+                        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                            <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                                <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                                    <table class="min-w-full divide-y divide-gray-200"">
+                                        <thead class=" bg-gray-50">
+                                        <tr>
+                                            <th scope="col"
+                                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                Date
+                                            </th>
+                                            <th scope="col"
+                                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                Total
+                                            </th>
+                                            <th scope="col" class="text-right relative px-6 py-3">
+                                                Invoice
+                                            </th>
+                                        </tr>
+                                        </thead>
+                                        <tbody class="bg-white divide-y divide-gray-200">
+                                            @foreach ($invoices as $invoice)
+                                            <tr>
+                                                <td
+                                                    class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                                                    {{ $invoice->date()->toFormattedDateString() }}
+                                                </td>
+                                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                                    {{ $invoice->total() }}
+                                                </td>
+                                                <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                                    <a href="/user/invoice/{{ $invoice->id }}"
+                                                        class="text-indigo-600 hover:text-indigo-900">
+                                                        Download
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                            @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </section>
             </div>
         </div>
